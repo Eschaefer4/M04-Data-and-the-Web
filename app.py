badad78f32b0bc4aa986350f3e170ed33c1bd749
eschaefer4 <eschaefer4@ivytech.edu>
2024-02-04 16:23:20 -0500

    Fixed flask error
    M04 case study now completed

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import sys
-sys.path.append('D:\\ivytech\\M04-Data-and-the-Web\\.venv\\Lib\\site-packages\\flask')
-sys.path.append('D:\\ivytech\\M04-Data-and-the-Web\\.venv\\Lib\\site-packages\\flask_sqlalchemy')
-sys.path.append('D:\\ivytech\\M04-Data-and-the-Web\\.venv\\Lib\\site-packages\\sqlalchemy')
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
+sys.path.append('.venv\\Lib\\site-packages\\')
 
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.app_context().push()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
@@ -18,6 +18,7 @@ class Book(db.Model):
     def __repr__(self):
         return f"{self.book_name} by {self.author}, published by {self.publisher}"
     
+db.create_all()
 
 @app.route('/')
 def index():
@@ -31,3 +32,24 @@ def get_books():
         book_data = {'book_name': book.book_name, 'author': book.author, 'publisher': book.publisher}
         output.append(book_data)
     return {"books": output}
+
+@app.route('/books/<id>')
+def get_book(id):
+    book = Book.query.get_or_404(id)
+    return jsonify({'book_name': book.book_name, 'author': book.author, 'publisher': book.publisher})
+
+@app.route('/books', methods=['POST'])
+def add_book():
+    book = Book(book_name=request.json['book_name'], author=request.json['author'], publisher=request.json['publisher'])
+    db.session.add(book)
+    db.commit()
+    return {"id":book.id}
+
+@app.route('/books/<id>', methods=['DELETE'])
+def delete_book():
+    book = Book.query.get(id)
+    if book is None:
+        return {"Error":"not found"}
+    db.session.delete(book)
+    db.session.commit()
+    return {"message":"deleted"}
